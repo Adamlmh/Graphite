2daf43362c78fdce6a1c2a123ebf125f09b06dbf
heisenberg-c1 <3151765378@qq.com>
2025-11-20 20:37:48 +0800

    feat(ui): 完成了固定工作栏的基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 // import TextProperties from './components/ui/business/Propertities/TextProperties/TextProperties';
 // import ShapeProperties from './components/ui/business/Propertities/ShapeProperties/ShapeProperties';
-import ImageProperties from './components/ui/business/Propertities/ImageProperties/ImageProperties';
+// import ImageProperties from './components/ui/business/Propertities/ImageProperties/ImageProperties';
+import ToolBar from './components/ui/business/ToolBar/ToolBar';
 function App() {
   return (
     <div>
@@ -9,8 +10,11 @@ function App() {
 
       <div>{/* <ShapeProperties /> */}</div>
 
-      <div>
+      {/* <div>
         <ImageProperties />
+      </div> */}
+      <div>
+        <ToolBar />
       </div>
     </div>
   );
