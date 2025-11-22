a98db3ebe0d771306f5a657f578b159e406cfaf6
Heisenberg11 <3151765378@qq.com>
2025-11-22 17:02:18 +0800

    Revert "feat(ui): 完成固定工作栏及属性面板基础样式"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import CanvasRenderer from './components/canvas/CanvasRenderer';
 import './lib/DOMEventBridge';
 import './App.css';
-// import TextProperties from './components/ui/business/Propertities/TextProperties/TextProperties';
-// import ShapeProperties from './components/ui/business/Propertities/ShapeProperties/ShapeProperties';
-// import ImageProperties from './components/ui/business/Propertities/ImageProperties/ImageProperties';
-// import ToolBar from './components/ui/business/ToolBar/ToolBar';
+
 function App() {
   return (
     <div className="app-container">
