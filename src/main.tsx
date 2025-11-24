4c7be4e5029e7e22ccba647ac6433133f62a7438
cunzhongyue <501087220@qq.com>
2025-11-24 21:58:19 +0800

    feat: 新增快捷键服务，在 main.tsx 中初始化

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { setupHotKeys } from './services/hotkeys/setupHotKeys';
+
+//初始化快捷键系统
+setupHotKeys();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
