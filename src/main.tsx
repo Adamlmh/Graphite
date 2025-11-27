c91c7c153a33b49fef95c85e6e40c8a43b9a77a1
cunzhongyue <501087220@qq.com>
2025-11-27 22:07:21 +0800

    fix(hotkey): 修复快捷键

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { setupHotKeys } from './services/hotkeys/setupHotKeys';
-import './services/hotkeys/testCanvasEvents';
+import { CreateInteraction } from './services/interaction/CreateInteraction';
 
 //初始化快捷键系统
 setupHotKeys();
+//初始化创建交互
+new CreateInteraction();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
