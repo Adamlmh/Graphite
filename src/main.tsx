c6a0ac30b1b39398db16a39b70c41af9cc167c15
cunzhongyue <501087220@qq.com>
2025-12-05 16:35:24 +0800

    feat(interaction): 实现调整大小和旋转功能集成historyservice服务

--- src/main.tsx
+++ src/main.tsx
@@ -2,36 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { setupHotKeys } from './services/hotkeys/setupHotKeys';
-<<<<<<< HEAD
-import { CreateInteraction } from './services/interaction/CreateInteraction';
-import { CopyPasteInteraction } from './services/interaction/CopyPasteInteraction';
-import { DeleteInteraction } from './services/interaction/DeleteInteraction';
-import { MoveInteraction } from './services/interaction/moveInteraction.ts';
-import { ResizeInteraction } from './services/interaction/ResizeInteraction';
-import { useCanvasStore } from './stores/canvas-store';
-import { HistoryService } from './services/HistoryService';
-
-//初始化快捷键系统
-setupHotKeys();
-// 全局初始化 HistoryService
-export const historyService = new HistoryService({
-  getState: () => useCanvasStore.getState(),
-  setState: (partial) => {
-    useCanvasStore.setState((state) => Object.assign(state, partial));
-  },
-});
-//初始化交互
-new CreateInteraction(historyService);
-export const copyPasteInteraction = new CopyPasteInteraction(historyService);
-export const deleteInteraction = new DeleteInteraction(historyService);
-export const moveInteraction = new MoveInteraction(historyService);
-export const resizeInteraction = new ResizeInteraction();
-=======
 import { historyService } from './services/instances';
 
 //初始化快捷键系统
 setupHotKeys();
->>>>>>> 58bc8d5 (fix: 修复main和app两个文件互相依赖问题)
 
 // 页面加载时恢复历史状态
 async function initApp() {
