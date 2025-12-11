87d06c35ec442f739699cec895490baed9ba1eca
qiujunkai <qiujunkai@xiaohongshu.com>
2025-12-11 19:30:49 +0800

    feat: 生产环境下除去console

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: packageJson.homepage || '/',
-});
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
+}));
